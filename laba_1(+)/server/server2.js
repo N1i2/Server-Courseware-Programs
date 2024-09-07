42f0b4449a19e2165f3c63d5346e280f7932e4fd
Nikolay Selicky <109921087+N1i2@users.noreply.github.com>
2024-09-07 08:23:24 +0300

    Do FirstLab

---
+++ laba_1(+)/server/server2.js
@@ -0,0 +1,86 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     const metod = req.method;
+//     const uri = req.uri;
+//     const version = req.httpVersion;
+//     const headers = req.headers;
+    
+//     let body = '';
+//     req.on('data', chank => {
+//         body += chank.toStreang();
+//     });
+
+//     req.on('end', () => {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+
+//         const responsHTML = `
+//         <html>
+//         <head>
+//             <title>Respons my server</title>
+//         </head>
+//         <body>
+//             <h1>Hello world</h1>
+//             <p><strong>Metod: </strong>${metod}</p>
+//             <p><strong>Uri: </strong>${uri}</p>
+//             <p><strong>HTTP version: </strong>${version}</p>
+//             <h2>Headers: </h2>
+//             <pre>${JSON.stringify(headers, null, 2)}</pre>
+//             <h2>Body: </h2>
+//             <pre>${body || 'Empty'}</pre>
+//         </body>
+//         </html>
+//         `;
+
+//         res.end(responsHTML);
+//     });
+// });
+
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//     console.log(`Server is running in port ${PORT}`);
+// })
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    const uri = req.url; 
+    const version = req.httpVersion;
+    const headers = req.headers;
+    
+    let body = '';
+    req.on('data', chunk => { 
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        const responseHTML = `
+        <html>
+        <head>
+            <title>Response from my server</title>
+        </head>
+        <body>
+            <h1>Hello world</h1>
+            <p><strong>Method: </strong>${method}</p>
+            <p><strong>URI: </strong>${uri}</p>
+            <p><strong>HTTP version: </strong>${version}</p>
+            <h2>Headers: </h2>
+            <pre>${JSON.stringify(headers, null, 2)}</pre>
+            <h2>Body: </h2>
+            <pre>${body || 'Empty'}</pre>
+        </body>
+        </html>
+        `;
+
+        res.end(responseHTML);
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
